4213de99143079bb6b7e7303dcdbda17ce7cb0f4
Emirton Sousa <emirtonf@gmail.com>
2023-06-27 20:22:31 -0300

    feat: service functions implemented.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './modules/home/home.module';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent
@@ -11,7 +12,8 @@ import { HomeModule } from './modules/home/home.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HomeModule
+    HomeModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/task-service.service.ts
+++ src/app/services/task-service.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment.development';
+import { Observable } from 'rxjs';
+import { Task } from '../modules/home/models/tasks';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskServiceService {
+  private apiBaseUrl = environment.apiBaseUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiBaseUrl);
+  };
+
+  public newTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.apiBaseUrl, task);
+  };
+
+  public deleteTask(taskName: String): Observable<Task> {
+    return this.http.delete<Task>(`${this.apiBaseUrl}/${taskName}`);
+  };
+
+  public updateTask(task: Task): Observable<Task> {
+    return this.http.put<Task>(this.apiBaseUrl, task);
+  };
+
+  public deleteAllTasks(): Observable<Task[]> {
+    return this.http.delete<Task[]>(this.apiBaseUrl);
+  };
 }
